ded2eaa71007204b7a21566d43aa3c837751326e
aline <linegcruz@gmail.com>
2022-12-15 21:41:20 -0300

    MailMap improvement

--- src/MailMap.java
+++ src/MailMap.java
@@ -1,20 +1,25 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class MailMap {
 
     HashMap<String, List<Mail>> mailBox = new HashMap<>();
 
     public void addMail(Mail mail) {
-        if (mailBox.containsKey(mail.getAddress())) {
-            mailBox.get(mail.getAddress()).add(mail);
+        if (mail != null) {
+            if (mailBox.containsKey(mail.getAddress())) {
+                mailBox.get(mail.getAddress()).add(mail);
+            } else {
+                List<Mail> mailList = new ArrayList<>();
+                mailList.add(mail);
+                mailBox.put(mail.getAddress(), mailList);
+            }
+            System.out.println(mail);
         } else {
-            List<Mail> mailList = new ArrayList<>();
-            mailList.add(mail);
-            mailBox.put(mail.getAddress(), mailList);
+            System.out.println("Não foi possível adicionar o email. Tente Novamente");
         }
-        System.out.println(mail);
     }
 
     public int totalAddress() {
@@ -23,12 +28,20 @@ public class MailMap {
 
     public int totalMailsOriginateFromAddress(String address) {
         int totalMails = 0;
-        for (String key: mailBox.keySet()) {
-            if(key == address) {
+        if (isAddressNullOrEmpty(address))
+            throw new IllegalArgumentException("Email inválido");
+
+        for (String key : mailBox.keySet()) {
+            if (Objects.equals(key, address)) {
                 totalMails = mailBox.get(key).size();
             }
         }
+
         return totalMails;
+    }
+
+    private boolean isAddressNullOrEmpty(String address) {
+        return address.isEmpty() || address.isBlank();
     }
 
     public void printMails() {
